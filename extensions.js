26d858c6cff1284d27ff96d210eced1509460ef9
Phil habz <philippe.habra@gmail.com>
2024-10-07 08:04:42 -0400

    revert to testing new feedback form commit

--- extensions.js
+++ extensions.js
@@ -469,6 +469,7 @@ export const FeedbackExtension = {
   match: ({ trace }) =>
     trace.type === 'ext_feedback' || trace.payload.name === 'ext_feedback',
   render: ({ trace, element }) => {
+<<<<<<< HEAD
     const feedbackContainer = document.createElement('div');
 
     // Define separate SVGs for thumb up and thumb down
@@ -538,28 +539,217 @@ export const FeedbackExtension = {
         </div>
       </div>
     `;
+=======
+    const feedbackContainer = document.createElement('div')
+
+    feedbackContainer.innerHTML = `
+          <style>
+            .vfrc-feedback {
+                display: flex;
+                align-items: center;
+                justify-content: space-between;
+            }
+
+            .vfrc-feedback--description {
+                font-size: 0.8em;
+                color: grey;
+                pointer-events: none;
+            }
+
+            .vfrc-feedback--buttons {
+                display: flex;
+            }
+
+            .vfrc-feedback--button {
+                margin: 0;
+                padding: 0;
+                margin-left: 0px;
+                border: none;
+                background: none;
+                opacity: 0.2;
+            }
+
+            .vfrc-feedback--button:hover {
+              opacity: 0.5; /* opacity on hover */
+            }
+
+            .vfrc-feedback--button.selected {
+              opacity: 0.6;
+            }
+
+            .vfrc-feedback--button.disabled {
+                pointer-events: none;
+            }
+
+            .vfrc-feedback--button:first-child svg {
+                fill: none; /* color for thumb up */
+                stroke: none;
+                border: none;
+                margin-left: 6px;
+            }
+
+            .vfrc-feedback--button:last-child svg {
+                margin-left: 4px;
+                fill: none; /* color for thumb down */
+                stroke: none;
+                border: none;
+                transform: rotate(180deg);
+            }
+          </style>
+          <div class="vfrc-feedback">
+            <div class="vfrc-feedback--description">Was this helpful?</div>
+            <div class="vfrc-feedback--buttons">
+              <button class="vfrc-feedback--button" data-feedback="1">${SVG_Thumb}</button>
+              <button class="vfrc-feedback--button" data-feedback="0">${SVG_Thumb}</button>
+            </div>
+          </div>
+        `
+>>>>>>> parent of b2d3d8d (testing new feedback form)
 
     feedbackContainer
       .querySelectorAll('.vfrc-feedback--button')
       .forEach((button) => {
-        button.addEventListener('click', function () {
-          const feedback = this.getAttribute('data-feedback');
+        button.addEventListener('click', function (event) {
+          const feedback = this.getAttribute('data-feedback')
           window.voiceflow.chat.interact({
             type: 'complete',
             payload: { feedback: feedback },
-          });
+          })
 
           feedbackContainer
             .querySelectorAll('.vfrc-feedback--button')
             .forEach((btn) => {
-              btn.classList.add('disabled');
+              btn.classList.add('disabled')
               if (btn === this) {
-                btn.classList.add('selected');
+                btn.classList.add('selected')
               }
-            });
-        });
-      });
+            })
+        })
+      })
 
+<<<<<<< HEAD
     element.appendChild(feedbackContainer);
   },
 };
+=======
+    element.appendChild(feedbackContainer)
+  },
+}
+
+export const FeedbackFormExtension = {
+  name: 'FeedbackForms',
+  type: 'response',
+  match: ({ trace }) =>
+    trace.type === 'ext_feedbackform' || trace.payload.name === 'ext_feedbackform',
+  render: ({ trace, element }) => {
+    const formContainer = document.createElement('form')
+
+    formContainer.innerHTML = `
+          <style>
+            label {
+              font-size: 0.8em;
+              color: #888;
+            }
+            input[type="text"], input[type="email"], input[type="tel"] {
+              width: 100%;
+              border: none;
+              border-bottom: 0.5px solid rgba(0, 0, 0, 0.1);
+              background: transparent;
+              margin: 5px 0;
+              outline: none;
+            }
+            .phone {
+              width: 150px;
+            }
+            .invalid {
+              border-color: red;
+            }
+            .submit {
+              background: linear-gradient(to right, #2e6ee1, #2e7ff1 );
+              border: none;
+              color: white;
+              padding: 10px;
+              border-radius: 5px;
+              width: 100%;
+              cursor: pointer;
+            }
+          </style>
+
+          <label for="name">Name</label>
+          <input type="text" class="name" name="name" required><br><br>
+
+          <label for="email">Email</label>
+          <input type="email" class="email" name="email" required pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" title="Invalid email address"><br><br>
+
+          <label for="phone">Phone Number</label>
+          <input type="tel" class="phone" name="phone" required pattern="\\d+" title="Invalid phone number, please enter only numbers"><br><br>
+
+          <input type="submit" class="submit" value="Submit">
+        `
+        formContainer.addEventListener('submit', function (event) {
+          event.preventDefault()
+    
+          const name = formContainer.querySelector('.name')
+          const email = formContainer.querySelector('.email')
+          const phone = formContainer.querySelector('.phone')
+    
+          if (
+            !name.checkValidity() ||
+            !email.checkValidity() ||
+            !phone.checkValidity()
+          ) {
+            name.classList.add('invalid')
+            email.classList.add('invalid')
+            phone.classList.add('invalid')
+            return
+          }
+    
+          formContainer.querySelector('.submit').remove()
+    
+          window.voiceflow.chat.interact({
+            type: 'complete',
+            payload: { name: name.value, email: email.value, phone: phone.value },
+          })
+        })
+    
+        element.appendChild(formContainer)
+      },
+    }
+
+
+export const URLExtension = {
+  name: 'OpenURL',
+  type: 'effect',
+  match: ({ trace }) =>
+    trace.type === 'ext_open_url' || trace.payload.name === 'ext_open_url',
+  effect: ({ trace }) => {
+    const url = trace.payload.url;
+    if (url) {
+      openURLInIframe(url);
+    } else {
+      console.error('No URL provided in the payload.');
+    }
+  },
+}
+
+// Helper function to open the URL in an iframe
+const openURLInIframe = (url) => {
+  let iframe = document.querySelector('#content-iframe');
+  if (!iframe) {
+    iframe = document.createElement('iframe');
+    iframe.id = 'content-iframe';
+    iframe.style.position = 'absolute';
+    iframe.style.top = '0';
+    iframe.style.left = '0';
+    iframe.style.width = '100vw';
+    iframe.style.height = '100vh';
+    iframe.style.zIndex = '1';
+    iframe.style.border = 'none';
+    document.body.appendChild(iframe);
+  }
+  iframe.src = url;
+}
+
+
+    
+>>>>>>> parent of b2d3d8d (testing new feedback form)
