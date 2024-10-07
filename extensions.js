319028361f09bc0e45532bbc7a46178cd584e782
Phil habz <philippe.habra@gmail.com>
2024-10-07 06:38:20 -0400

    fix on feedbackform extension

--- extensions.js
+++ extensions.js
@@ -657,38 +657,5 @@ export const FeedbackFormExtension = {
       },
     }
 
-export const URLExtension = {
-  name: 'OpenURL',
-  type: 'effect',
-  match: ({ trace }) =>
-    trace.type === 'ext_open_url' || trace.payload.name === 'ext_open_url',
-  effect: ({ trace }) => {
-    const url = trace.payload.url;
-    if (url) {
-      openURLInIframe(url);
-    } else {
-      console.error('No URL provided in the payload.');
-    }
-  },
-}
-
-// Helper function to open the URL in an iframe
-const openURLInIframe = (url) => {
-  let iframe = document.querySelector('#content-iframe');
-  if (!iframe) {
-    iframe = document.createElement('iframe');
-    iframe.id = 'content-iframe';
-    iframe.style.position = 'absolute';
-    iframe.style.top = '0';
-    iframe.style.left = '0';
-    iframe.style.width = '100vw';
-    iframe.style.height = '100vh';
-    iframe.style.zIndex = '1';
-    iframe.style.border = 'none';
-    document.body.appendChild(iframe);
-  }
-  iframe.src = url;
-}
-
 
     
